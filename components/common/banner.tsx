af0a80edd5f2ddc9924d2624873587d956ef97ef
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2024-04-10 02:46:32 +0300

    rich text

--- components/common/banner.tsx
+++ components/common/banner.tsx
@@ -23,6 +23,7 @@ const animateItem = { variants: item };
 
 export const Banner: FC<PropsWithChildren> = ({ children }) => {
   const t = useTranslations("Home");
+
   return (
     <motion.section
       className="text-center pb-20 pt-10 relative"
@@ -30,44 +31,47 @@ export const Banner: FC<PropsWithChildren> = ({ children }) => {
       initial="hidden"
       animate="show"
     >
-      <div className="green-background" aria-description="image wrapper">
-        </div>
-        <div className="font-bold" id="about">
-          <motion.h1 className="text-9xl pb-2" {...animateItem}>
-            {t("name")}
-          </motion.h1>
-          <motion.h2
-            className="text-7xl text-primary text-balance pb-4"
-            {...animateItem}
-          >
-            {t("welcome-1")}
-          </motion.h2>
-          <motion.h3 className="text-7xl leading-tight" {...animateItem}>
-            {t("welcome")}
-          </motion.h3>
-        </div>
-        <motion.p
-          className="text-balance banner-p pt-12 font-semibold"
+      <div className="green-background" aria-description="image wrapper"></div>
+      <div className="font-bold" id="about">
+        <motion.h1 className="text-9xl pb-2" {...animateItem}>
+          {t("name")}
+        </motion.h1>
+        <motion.h2
+          className="text-7xl text-primary text-balance pb-4"
           {...animateItem}
         >
-          {t("about.0")}
-          <a href="https://www.typescriptlang.org" target="_blank">
-            Typescript
-          </a>
-          ,{t("with")}
-          <a href="https://nextjs.org" target="_blank">
-            NextJs
-          </a>
-          {t("about.1")}
-          <a href="https://nodejs.org" target="_blank">
-            NodeJs
-          </a>
-          {t("about.2")}
-          <a href="https://aws.amazon.com" target="_blank">
-            Amazon Web Services
-          </a>
-          {t("about.3")}.{t("about.4")}
-        </motion.p>
+          {t("welcome-1")}
+        </motion.h2>
+        <motion.h3 className="text-7xl leading-tight" {...animateItem}>
+          {t("welcome")}
+        </motion.h3>
+      </div>
+      <motion.p
+        className="text-balance banner-p pt-12 font-semibold"
+        {...animateItem}
+      >
+        {t.rich("about.intro", {
+          strong: (chunks) => <strong>{chunks}</strong>,
+        })}
+      </motion.p>
     </motion.section>
   );
 };
+// {t("about.0")}
+//          <a href="https://www.typescriptlang.org" target="_blank">
+//            Typescript
+//          </a>
+//          ,{t("with")}
+//          <a href="https://nextjs.org" target="_blank">
+//            NextJs
+//          </a>
+//          {t("about.1")}
+//          <a href="https://nodejs.org" target="_blank">
+//            NodeJs
+//          </a>
+//          {t("about.2")}
+//          <a href="https://aws.amazon.com" target="_blank">
+//            Amazon Web Services
+//          </a>
+//          {t("about.3")}.{t("about.4")}
+//
